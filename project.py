010290846e34f8520d935d9179a969c5b5a982ed
Jeff7531 <jeffmkwang@gmail.com>
2023-02-16 19:21:23 -0800

    added image

--- project.py
+++ project.py
@@ -14,5 +14,15 @@ def home():
 def contact():
     return render_template("contact.html")
 
+
+
+@app.route('/hobbies')
+def hobbies():
+    return render_template("hobbies.html")
+
+@app.route("/experience")
+def experience():
+    return render_template("experience.html")
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
