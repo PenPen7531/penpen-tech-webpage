e977cf9b2d9cc0618a62b42c8e09d12962f02d8b
Jeff7531 <jeffmkwang@gmail.com>
2023-02-19 16:17:35 -0800

    Email SMTP

--- project.py
+++ project.py
@@ -1,8 +1,15 @@
+import os
 from flask import Flask, render_template, redirect, request
-
-
+from email.message import EmailMessage
+import ssl
+import smtplib
 
 app = Flask(__name__)
+
+email_sender = "penpentech000@gmail.com"
+email_password = "xiwknwzqmyutqelt"
+email_receiver = "jeffmkwang@gmail.com"
+
 
 
 @app.route("/")
@@ -10,11 +17,42 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/contact")
+@app.route("/contact", methods = ["GET", "POST"])
 def contact():
-    return render_template("contact.html")
 
+    if request.method == "GET":
 
+        return render_template("contact.html")
+    
+    else:
+        name = request.form.get("name")
+        
+        email = request.form.get('email')
+        
+        comment = request.form.get('comment')
+
+        
+
+        subject = f"PenPen.tech New Email from {name}, {email}"
+        body = f"Name: {name}\nEmail: {email}\n{comment}"
+        print(body)
+        
+        # Email Stuff
+        em = EmailMessage()
+        em['From'] = email_sender
+        em["To"] = email_receiver
+        em["Subject"] = subject
+        em.set_content(body)
+
+        # SSL
+        context = ssl.create_default_context()
+
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+            smtp.login(email_sender, email_password)
+            smtp.sendmail(email_sender, email_receiver, em.as_string())
+
+        
+        return render_template('contact.html')
 
 @app.route('/hobbies')
 def hobbies():
