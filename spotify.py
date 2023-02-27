70c797add132b690467af57143a01e6efdd51d77
PenPen7531 <jeffmkwang@gmail.com>
2023-02-26 21:57:22 -0800

    spotify config and yml

--- spotify.py
+++ spotify.py
@@ -1,1 +1,11 @@
-client = "ddcea641ed8144de903237b83273e4b1"
+import yaml
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+with open('app_conf.yml', 'r') as f:
+    app_config = yaml.safe_load(f.read())
+
+sp = spotipy.Spotify(auth_manager = SpotifyOAuth(client_id = app_config['client_id'],
+                                                client_secret = app_config['client_secret'],
+                                                redirect_uri = app_config['redirect_uri'],
+                                                scope = app_config['scope']))
